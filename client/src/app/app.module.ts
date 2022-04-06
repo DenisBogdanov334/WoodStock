afc495ee8776c652b26d5a0dbd90459409a61f0d
Denis Bogdanov <denis.i.bogdanov334@gmail.com>
2022-04-07 01:35:34 +0200

    Added a basic nav-bar
    Initial nav-bar. Also changes were made to font awesome to work with Angular so I adjusted the project to be compatible with it. Icons were not showing before the changes.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,17 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, NavBarComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FontAwesomeModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
